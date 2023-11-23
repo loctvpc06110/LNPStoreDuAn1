9b41aef94b87b150ad54669b84cfcff1e490b3f0
loctvpc06110 <loctvpc06110@gmail.com>
2023-11-23 12:12:03 +0700

    N01 - Upload Giao Diện

---
+++ admin/products/product.php
@@ -0,0 +1,24 @@
+<?php 
+// code class Products
+// Khai báo thuộc tính của Product
+
+class Product {
+    var $id = null;
+    var $name = null;
+    var $status = null;
+    var $image = null;
+    var $price = null;
+    var $view = null;
+    var $promoID = null;
+    var $cateID = null;
+
+   
+    public function getListDetail() {
+        $db = new connect();
+        $query = "SELECT * FROM products INNER JOIN detail_prod ON products.prod_id = detail_prod.prod_id"; // viết câu lệnh sql 
+        $result = $db->pdo_query($query);
+        return $result;
+    }
+
+}
+?>
---
+++ site/includes/_feature.php
@@ -0,0 +1,26 @@
+<section id="feature" class="section-p1">
+        <div class="fe-box">
+            <img src="images/feature/f1.png">
+            <h6>Miễn Phí Vận Chuyển</h6>
+        </div>
+        <div class="fe-box">
+            <img src="images/feature/f2.png">
+            <h6>Đặt Hàng Trực Tuyến</h6>
+        </div>
+        <div class="fe-box">
+            <img src="images/feature/f3.png">
+            <h6>Tiết Kiệm Tiền Bạc</h6>
+        </div>
+        <div class="fe-box">
+            <img src="images/feature/f4.png">
+            <h6>Khuyến Mãi Hấp Dẫn</h6>
+        </div>
+        <div class="fe-box">
+            <img src="images/feature/f5.png">
+            <h6>Thuận Mua Vừa Bán</h6>
+        </div>
+        <div class="fe-box">
+            <img src="images/feature/f6.png">
+            <h6>Hỗ Trợ Khách Hàng 24/7</h6>
+        </div>
+</section>
