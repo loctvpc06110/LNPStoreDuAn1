d2244a249bb3661b4d756a59ae27aaba7ea8d49d
LAPTOP ASUS <nhannghiatran2@gmail.com>
2023-12-13 21:31:18 +0700

    NO-2

--- admin/includes/pdo.php
+++ admin/includes/pdo.php
@@ -3,9 +3,9 @@ class connect
 {
     function pdo_get_connection()
     {
-        $dburl = "mysql:host=localhost;dbname=lnp_store_final;charset=utf8";
+        $dburl = "mysql:host=localhost;dbname=lnp-store1;charset=utf8";
         $username = 'root';
-        $password = '';
+        $password = 'mysql';
         $conn = new PDO($dburl, $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
